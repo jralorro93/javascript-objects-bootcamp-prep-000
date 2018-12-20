690c075a1b85644215fda5ba60bfe2ac66def421
Juan Alorro <jralorro93@gmail.com>
2018-12-20 18:42:20 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,7 +6,11 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
+<<<<<<< HEAD
   delete playlist[artistName];
+=======
+  delete playlist.artistName;
+>>>>>>> 5f19e9c36f3851833adf232f43959682cef45a66
   return playlist;
 }
 
